808dec6229730a3cf909014db2a9a278e522f201
John Uthus <john.a.uthus.jr1@gmail.com>
2020-09-01 19:51:40 -0700

    Add project files.

---
+++ CRUDWithIssuesCore/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDWithIssuesCore.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+
+        public string Name { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+    }
+}
---
+++ CRUDWithIssuesCore/Models/StudentDb.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDWithIssuesCore.Models
+{
+    public static class StudentDb
+    {
+        public async static Task<Student> Add(Student p, SchoolContext db)
+        {
+            //Add student to context
+            db.Students.Add(p);
+            await db.SaveChangesAsync();
+            return p;
+        }
+
+        public async static Task<List<Student>> GetStudents(SchoolContext context)
+        {
+            return await (from s in context.Students
+                          orderby s.Name ascending
+                          select s).ToListAsync();
+        }
+
+        public async static Task<Student> GetStudent(SchoolContext context, int id)
+        {
+            Student p2 = await (from students in context.Students
+                          where students.StudentId == id
+                          select students).SingleAsync();
+            return p2;
+        }
+    }
+}
